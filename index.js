749a89acb2f1cc4292758bcf9456214e8dd3e8f5
ramshankarb <4612sainik@gmail.com>
2018-06-30 22:06:16 +0530

    First-commit

---
+++ index.js
@@ -0,0 +1,50 @@
+$(document).ready(() => {
+    $("#submitBtn").click(() => {
+        getAllDetails(); //making the call
+    })  
+}); // end document.ready function
+let myUrl=null;
+let getAllDetails = () => {
+    // API call to get user details
+    let year=title=imdbId="";
+    //setting value
+    year =$("#imdbYear").val();
+    title= $("#imdbTitle").val();
+    imdbId = $("#imdbID").val();
+
+    //Setting the URL Logic
+    if((year !="" || year!=null)&&(title=="" || title==null)&&(imdbId==""||imdbId==null)){
+        let title2= window.prompt("Title is required");
+        myUrl = "http://www.omdbapi.com/?t="+title2+"&y="+year+"&apikey=280d032";
+    }
+    else
+    myUrl = "http://www.omdbapi.com/?i="+imdbId+"&t="+title+"&y="+year+"&apikey=280d032"; 
+    
+    $.ajax({
+        type:'GET',
+        dataType:'json',
+        async:true,
+        url:myUrl,
+        success: (response) => {
+            //console.log(myUrl);//For testing purpose
+            let output = `  <br>
+                            <div class="media">
+                            <img class="media-left" alt="image is NA" src="${response.Poster}">
+                            <div class="media-body">
+                            <h4 class="card-title">${response.Title}</h4>
+                            <p class="card-text">${response.Genre}</p>
+                            <p class="card-text">${response.Director}</p>
+                            <p class="card-text">${response.Plot}</p>
+                            </div>
+                            </div> `
+            
+            $("#showData").append(output); //attaching output
+        }, 
+        error: (err) => {
+            console.log(err.responseJSON.error.message);
+            alert(err.responseJSON.error.message);
+        }
+
+    });// end ajax call 
+
+}
